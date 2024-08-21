52c9c72d776fc45e30175af94f2ea02e3c4b74db
pellekrab <78560773+PelleKrab@users.noreply.github.com>
2024-08-20 18:10:59 -0600

    Edited thumbnail for eco bites

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -7,35 +7,35 @@ const Projects: React.FC = () => (
       <h2 className="text-3xl font-bold">My Projects</h2>
       <p>An incomplete collection of things I have worked on over the years. <br />Click on a project to learn more!</p>
     </header>
-    <ProjectEntry 
-      href="/ECOBitesBSWHackathon" 
-      title="ECO Bites Boulder Startup Week Hackathon" 
-      description="A hackathon project aimed at promoting sustainability within the restaurant industry. Our team developed a React-based website that uses AI to rate restaurants on their sustainability practices." 
-      technologies="React, TypeScript, nextJS, AI, Claude 3, and AWS" 
+    <ProjectEntry
+      href="/ECOBitesBSWHackathon"
+      title="ECO Bites Boulder Startup Week Hackathon"
+      description="A hackathon project aimed at promoting sustainability within the restaurant industry. Our team developed a React-based website that uses AI to rate restaurants on their sustainability practices."
+      technologies="React, TypeScript, nextJS, AI, Claude 3, and AWS"
     />
-    <ProjectEntry 
-      href="/rust-blockchain" 
-      title="Rust Blockchain" 
-      description="A Rust-based project focused on teaching me the basics of blockchain, multithreading, and cryptography. It highlights key blockchain functionalities such as transaction processing, block mining, and transaction validation, offering a practical introduction to these foundational technologies." 
-      technologies="Rust, Multithreading, and Blockchain" 
+    <ProjectEntry
+      href="/rust-blockchain"
+      title="Rust Blockchain"
+      description="A Rust-based project focused on teaching me the basics of blockchain, multithreading, and cryptography. It highlights key blockchain functionalities such as transaction processing, block mining, and transaction validation, offering a practical introduction to these foundational technologies."
+      technologies="Rust, Multithreading, and Blockchain"
     />
-    <ProjectEntry 
-      href="/frc-java-framework" 
-      title="FRC Java Framework" 
-      description="A command-based framework that allows teams to easily program their robot using XML for FIRST Robotics Competition (FRC). This framework is designed to simplify the programming process and make it easier for teams to focus on building their robot." 
-      technologies="Java, OpenCV, PID, and WPILib" 
+    <ProjectEntry
+      href="/frc-java-framework"
+      title="FRC Java Framework"
+      description="A command-based framework that allows teams to easily program their robot using XML for FIRST Robotics Competition (FRC). This framework is designed to simplify the programming process and make it easier for teams to focus on building their robot."
+      technologies="Java, OpenCV, PID, and WPILib"
     />
-    <ProjectEntry 
-      href="/java-robotics-project" 
-      title="FRC Rapid React" 
-      description="This project marked my debut in the world of the First Robotics Competition (FRC) and Java programming. While it may not represent my finest code, it enabled us to compete at a high level during our FRC regionals." 
-      technologies="Java, OpenCV, and WPILib" 
+    <ProjectEntry
+      href="/java-robotics-project"
+      title="FRC Rapid React"
+      description="This project marked my debut in the world of the First Robotics Competition (FRC) and Java programming. While it may not represent my finest code, it enabled us to compete at a high level during our FRC regionals."
+      technologies="Java, OpenCV, and WPILib"
     />
-    <ProjectEntry 
-      href="/space-shooter-project" 
-      title="Multiplayer Space Shooter" 
-      description="Asteroids inspired, multiplayer web game. Players control a ship with a basic cannon for combat. This project was created in collaboration with Will Franco and Zachary Zabel for a sophomore year project." 
-      technologies="JavaScript, CSS, HTML, Socket.io, and web hosting" 
+    <ProjectEntry
+      href="/space-shooter-project"
+      title="Multiplayer Space Shooter"
+      description="Asteroids inspired, multiplayer web game. Players control a ship with a basic cannon for combat. This project was created in collaboration with Will Franco and Zachary Zabel for a sophomore year project."
+      technologies="JavaScript, CSS, HTML, Socket.io, and web hosting"
     />
   </section>
 );
